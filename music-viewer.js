6273b02ded7212d5f5aa38a2f207cf5b61ae35a4
Andrew Abbott <mirrexagon@mirrexagon.com>
2019-03-14 18:44:33 +1100

    Make basic music list

--- music-viewer.js
+++ music-viewer.js
@@ -1,22 +1,47 @@
 const CONTENT_URL = "https://s3-ap-southeast-2.amazonaws.com/lmirx.net";
 
 const MUSIC_CONTENT_URL = CONTENT_URL + "/music";
-const MUSIC_METADATA_URL = MUSIC_CONTENT_URL + "/metadata.json"
+const MUSIC_DATA_URL = MUSIC_CONTENT_URL + "/music.json"
 
 const CONTAINER_ID = "music-viewer";
 
+const MUSIC_LIST_CLASS = "music-list";
+const SONG_ITEM_CLASS = "song-item";
+
 function main() {
-    get_music_metadata()
-    .then(metadata => {
-        document.getElementById(CONTAINER_ID).innerHTML = "Coming soon.";
+    get_music_data()
+    .then(music_data => {
+        document.getElementById(CONTAINER_ID).appendChild(make_list_from_music_data(music_data));
     })
     .catch(err => {
         console.log(err);
     });
 }
 
-async function get_music_metadata() {
-    let json_string = await fetch(MUSIC_METADATA_URL);
+function make_list_from_music_data(music_data) {
+    let list = document.createElement("ul");
+    list.className = MUSIC_LIST_CLASS;
+
+    for (let i = 0; i < music_data.length; ++i) {
+        let item = document.createElement('li');
+        item.appendChild(make_song_item(music_data[i]));
+        list.appendChild(item);
+    }
+
+    return list;
+}
+
+function make_song_item(song_data) {
+    let item = document.createElement("div");
+    item.className = SONG_ITEM_CLASS;
+
+    item.appendChild(document.createTextNode(song_data.artist + " - " + song_data.title));
+
+    return item;
+}
+
+async function get_music_data() {
+    let json_string = await fetch(MUSIC_DATA_URL);
     return json_string.json();
 }
 
