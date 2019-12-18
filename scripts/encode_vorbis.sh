b6f20e5ef4eb1686028cb75c69c3b57cda3efbde
Andrew Abbott <mirrexagon@mirrexagon.com>
2019-12-18 21:37:48 +1100

    Add music management scripts

---
+++ scripts/encode_vorbis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+MUSIC_DIR=~/archive/finished_music/music
+
+process_limit=16
+
+# ---
+
+pushd ${MUSIC_DIR}
+
+for src in *.flac; do
+    OGG_PATH=$(echo ${src} | sed 's/\.flac$/\.ogg/')
+    if [ ! -e $OGG_PATH ]; then
+        ffmpeg -y -i ${src} $OGG_PATH &
+    fi
+
+    while test $(pgrep -cx ffmpeg) -ge ${process_limit}; do
+        sleep 1
+    done
+done
+
+popd
