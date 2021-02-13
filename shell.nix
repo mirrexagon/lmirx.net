0ea77f7091f2eac2ed146f852aad52b1753c41ac
Andrew Abbott <mirrexagon@mirrexagon.com>
2021-02-13 19:44:56 +1100

    Add lume to shell.nix

--- shell.nix
+++ shell.nix
@@ -6,5 +6,12 @@ pkgs.mkShell {
     python3Packages.python
     python3Packages.mutagen
     awscli
+    deno
   ];
+
+  shellHook = ''
+    lume() {
+      deno run --unstable -A https://deno.land/x/lume@v0.15.0/cli.js "$@"
+    }
+  '';
 }
